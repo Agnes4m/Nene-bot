b8e561d31232cc6f6807b7914523e1599c29fec0
Agnes Digital <Z735803792@163.com>
2023-07-27 09:17:17 +0800

    更新pre-commit

--- bot.py
+++ bot.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import nonebot
-from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-
 from nonebot.adapters.kaiheila import Adapter as 开黑啦Adapter
-
-from nonebot.adapters.qqguild import Adapter as QQ_频道Adapter
-
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 from nonebot.adapters.onebot.v12 import Adapter as ONEBOT_V12Adapter
-
-
+from nonebot.adapters.qqguild import Adapter as QQ_频道Adapter
 
 nonebot.init()
 
@@ -22,7 +17,7 @@ driver.register_adapter(QQ_频道Adapter)
 
 driver.register_adapter(ONEBOT_V12Adapter)
 
-nonebot.load_builtin_plugins('echo')
+nonebot.load_builtin_plugins("echo")
 
 
 nonebot.load_from_toml("pyproject.toml")
